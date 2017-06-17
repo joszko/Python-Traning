bfb45799cc16c7f936de2b8cb62844d442ce73c4
joszko <joszko@gmail.com>
2017-06-17 17:24:34 +0200

    .

--- OOP_tutorial.py
+++ OOP_tutorial.py
@@ -40,8 +40,45 @@ class Employee:
         return True
 
 
-emp_1 = Employee('Marcin','Brzeski',60000)
+# inheriting from the class Employee with all of it's functionality
+class Developer(Employee):
+
+    raise_amount = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first,last,pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first,last,pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self,emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->',emp.fullname())
+
+emp_1 = Employee('Marcin', 'Brzeski', 60000)
 emp_2 = Employee('test','user',50000)
 
-my_date = datetime.date(2016, 7, 10)
-print(Employee.is_workday(my_date))
+# my_date = datetime.date(2016, 7, 10)
+# print(Employee.is_workday(my_date))
+
+dev_1 = Developer('Marcin','Brzeski',60000,'Python')
+
+mgr = Manager('Marcin', 'Brzeski', 60000, [emp_2])
+
+mgr.print_emps()
